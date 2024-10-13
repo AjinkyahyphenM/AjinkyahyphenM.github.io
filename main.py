ed263116656b15ca6d101bd670327cfbe6400b41
AjinkyahyphenM <96536389+AjinkyahyphenM@users.noreply.github.com>
2024-10-14 01:31:41 +0530

    first major release

--- main.py
+++ main.py
@@ -1,19 +1,61 @@
-from pyscript import ffi, js_modules, document
-import code
+from pyscript import document, window, ffi, when
+from js import console
+from shell import loadTerminal
 
-# addon = js_modules.weblinks.FitAddon.new()
-__terminal__.options = ffi.to_js({"theme":
-                                        {"background":"white"},
-                                    "minimumContrastRatio":6,
-                                    "cursorStyle":"underline",
-                                    "cursorBlink":True,
-                                    # "allowTransparency":False,
-                                    # "windowOptions":
-                                    #     {"setWinSizePixels":True}
-                                    })
-# __terminal__.loadAddon(addon)
-__terminal__.write("\x9D12;#828181\x07")
-# __terminal__.write("\x9B4;10;10t")
-# addon.fit()
 
-code.interact()
+ROOT = document.querySelector(':root')
+STY = window.getComputedStyle(ROOT)
+
+def loadState(event = None):
+    bg_light = STY.getPropertyValue('--bg_light')
+    fg_light = STY.getPropertyValue('--fg_light')
+    bg_dark = STY.getPropertyValue('--bg_dark')
+    fg_dark = STY.getPropertyValue('--fg_dark')
+    theme = window.localStorage.getItem("theme")
+    console.log("current theme: "+window.localStorage.getItem("theme"))
+    curr_theme_ind = document.getElementById("theme_toggle_btn")
+    if theme == "dark":
+        curr_theme_ind.innerHTML = "!Dark"
+        ROOT.style.setProperty("--bg_col", bg_dark)
+        ROOT.style.setProperty("--fg_col", fg_dark)
+    elif theme == "light":
+        curr_theme_ind.innerHTML = "Light!"
+        ROOT.style.setProperty("--bg_col", bg_light)
+        ROOT.style.setProperty("--fg_col", fg_light)
+    else:
+        curr_theme_ind.innerHTML = "!Dark"
+        ROOT.style.setProperty("--bg_col", bg_dark)
+        ROOT.style.setProperty("--fg_col", fg_dark)
+
+def toggleTheme(event):
+    bg_light = STY.getPropertyValue('--bg_light')
+    fg_light = STY.getPropertyValue('--fg_light')
+    bg_dark = STY.getPropertyValue('--bg_dark')
+    fg_dark = STY.getPropertyValue('--fg_dark')
+    theme = window.localStorage.getItem("theme")
+    curr_theme_ind = document.getElementById("theme_toggle_btn")
+    if theme == 'dark':
+        console.log("dark set to light")
+        window.localStorage.setItem("theme", "light")
+        curr_theme_ind.innerHTML = "Light!"
+        ROOT.style.setProperty("--bg_col", bg_light)
+        ROOT.style.setProperty("--fg_col", fg_light)
+    elif theme == 'light':
+        console.log("light set to dark")
+        window.localStorage.setItem("theme", "dark")
+        console.log("current theme: "+window.localStorage.getItem("theme"))
+        curr_theme_ind.innerHTML = "!Dark"
+        ROOT.style.setProperty("--bg_col", bg_dark)
+        ROOT.style.setProperty("--fg_col", fg_dark)
+    else:
+        console.log("default")
+        window.localStorage.setItem("theme", "dark")
+        console.log("current theme: "+window.localStorage.getItem("theme"))
+        curr_theme_ind.innerHTML = "!Dark"
+        ROOT.style.setProperty("--bg_col", bg_dark)
+        ROOT.style.setProperty("--fg_col", fg_dark)
+    loadTerminal()
+
+
+if __name__ == "__main__":
+    loadState()
